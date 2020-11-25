5d6122f6e3dd5c773ec9e296e7ff59a3cbf002b5
acorreiaa <amandaluana777@gmail.com>
2020-11-25 07:37:36 -0300

    Feat JS DataType Strings

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,61 @@
-alert('Hello, Guys')
+/* Inicio JavaScript - 23/11/2020
+// strings
+console.log('Hello, Guys');
+
+let email = 'luzi@thenetninja.co.uk';
+console.log(email);
+
+// string concatenation
+let firstName = 'Brandon';
+let lastName = 'Sanderson';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// getting characteres
+console.log(fullName[2]);
+
+// string length
+console.log(fullName.length);
+
+// string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf('@');
+console.log(index);
+
+*/
+
+/*
+//commom string methods = 25/11/2020
+let email = 'mario@thenetninja.co.uk';
+
+// let result1 = email.lastIndexOf('n');
+
+// let result1 = email.slice(2,10);
+
+// let result1 = email.substr(4,10);
+
+// let result1 = email.replace('m', 'w');
+
+// let result1 = email.replace('n', 'w');   Nota: Quando há mais de uma variavel, altera APENAS a pimeira que encontrar
+
+console.log(result1);
+
+*/
+
+// NUMEROS
+let radius = 10;
+const pi = 3.14;
+
+//console.log(radius, pi);
+
+//math operations: +, -, /, **, %
+
+//console.log(10/2);
+//let result2 = radius % 3;
+let result2 = pi * radius**2;
+console.log(result2);
+
