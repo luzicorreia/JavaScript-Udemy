861decef9b5c26576c56bfc74050cdb868f97491
acorreiaa <amandaluana777@gmail.com>
2020-11-30 07:40:39 -0300

    Feat JS DataType Arrays

---
+++ sandbox_Template12-13.js
@@ -0,0 +1,47 @@
+// Template strings
+// Template 12
+//const title = 'Best reads of 2019';
+//const author = 'Mario';
+//const likes12 = 30;
+
+// concatenation way
+// let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes12 + ' likes';
+// console.log(result);
+
+// template strings way
+//let result = `The blog called ${title} by ${author} has ${likes12} likes`; 
+//console.log(result);
+
+// creating html templates
+//let html = `
+//<h2>${title}</h2>
+//<p>By ${author}>/p>
+//<span>This blog has ${likes} likes</span>
+//`;
+//console.log(html);
+
+// Template 13 - ARRAYS
+let ninjas = ['shaun', 'ryu', 'chun-li'];
+//console.log(ninjas[1];);
+//ninjas[1] = 'ken'
+//console.log(ninjas[1]);
+
+//let ages = [20,25,30,35];
+//console.log(ages[2]);
+
+//let random = ['shaun', 'crystal, 20,30'];
+//console.log(random);
+
+// ARRAY METHODS
+//let result = ninjas.join('/');    /* Junta conteudo da variavel separado por barra (/) - Pode usar outros separadores
+//console.log(result);
+
+//let result = ninjas.indexOf('chun-li') /* Busca indice do array /*
+//console.log(result);
+
+//let result = ninjas.concat(['ken', 'cristal']); /* Contatena (soma) novos dados ao ARRAY acima(ninjas) /*
+//console.log(result);
+
+//let result = ninjas.push('ken'); 
+//result = ninjas.pop();
+//console.log(ninjas);   /*PUSH: acrescenta item na lista; POP: retira item da lista */
