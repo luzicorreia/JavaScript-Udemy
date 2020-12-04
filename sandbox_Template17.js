2780df879ee66b1137524d474b73f0fc6f180ba7
acorreiaa <amandaluana777@gmail.com>
2020-12-04 07:32:48 -0300

    Feat JS DataType Comparasion Conversion

---
+++ sandbox_Template17.js
@@ -0,0 +1,25 @@
+// TYPE CONVERSION
+
+//let score = '100';
+
+//score = Number(score); 
+//*Sem informar o tipo da variável (Number), considera string e concatena ('100' + 1 = 1001). Number = 100 + 1 = 101 
+
+//console.log(score + 1);
+//console.log(typeof score); /*Exibe o tipo da variavel, no caso NUMBER
+
+//let result5 = Number('hello');
+//console.log(result5);    /* REsultado: NaN(Not a Number)
+
+//let result5 = String(50);
+//console.log(result5, typeof result5);
+
+// BOOLEAN
+// => Conversão de um dado (ZERO ou String vazia (não tem SPACE para Boolean = FAlSO. O resto é TRUE)
+//let result5 = Boolean(0);     /* FALSE
+//let result5 = Boolean(10);    /* TRUE
+//let result5 = Boolean(-10);   /* TRUE
+//let result5 = Boolean('');    /* FALSE
+let result5 = Boolean(' ');     /* TRUE */
+
+console.log(result5, typeof result5);
